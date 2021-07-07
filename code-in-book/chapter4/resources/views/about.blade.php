7e5a0f6aa105b68e479e5cfd69ff35c7ff83cb98
Saac Sornchai <sornchai.la@ku.th>
2021-07-07 12:01:20 +0700

    4.1

---
+++ code-in-book/chapter4/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="lg:flex lg:items-center lg:justify-between">
+        <div class="flex-1 min-w-0">
+            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                About us
+            </h2>
+        </div>
+    </div>
+    <div>
+        <p>My App is Demo App</p>
+    </div>
+@endsection
---
+++ code-in-book/chapter4/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        colors: {
+            ...colors,
+            "current": "current",
+            "transparent": "transparent",
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
